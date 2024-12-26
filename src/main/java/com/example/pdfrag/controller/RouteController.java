c648e63429e3cd8fe28e6b8267c29d031eef0fd4
parkmaeil <bitcocom@empas.com>
2024-12-26 17:34:57 +0900

    ragpdf

--- src/main/java/com/example/pdfrag/controller/RouteController.java
+++ src/main/java/com/example/pdfrag/controller/RouteController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RouteController {
 
-    @GetMapping("/pdfReg")
+    @GetMapping("/pdfRag")
     public String korea(){
-        return "rag";
+        return "rag"; // rag.html
     }
 }
